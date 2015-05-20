0aae545d3a01fbdf6fc91dbd8b2e0f9819ca6f60
RestyArtiaga <resty.artiaga@gmail.com>
2015-05-20 17:14:56 +0800

    replaced sql file

--- spring.sql
+++ spring.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2015 at 12:47 PM
+-- Generation Time: May 20, 2015 at 11:14 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -32,14 +32,16 @@ CREATE TABLE IF NOT EXISTS `bidhistory` (
   `itemID` int(11) NOT NULL,
   `price` double NOT NULL,
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `bidhistory`
 --
 
 INSERT INTO `bidhistory` (`bidID`, `userID`, `itemID`, `price`, `createdAt`) VALUES
-(33, 7, 41, 20000, '2015-05-14 18:47:16');
+(33, 7, 41, 20000, '2015-05-14 18:47:16'),
+(34, 6, 41, 20001, '2015-05-18 17:39:57'),
+(35, 6, 45, 20, '2015-05-20 16:43:39');
 
 -- --------------------------------------------------------
 
@@ -54,14 +56,17 @@ CREATE TABLE IF NOT EXISTS `item` (
   `duration` int(11) DEFAULT NULL,
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `userID` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `item`
 --
 
 INSERT INTO `item` (`itemId`, `itemName`, `itemDescription`, `duration`, `createdAt`, `userID`) VALUES
-(41, 'Asus Laptop', 'A very sturdy laptop.', 24, '2015-05-14 18:47:15', 7);
+(41, 'ASUS', 'A laptop worth buying.', 20, '2015-05-14 18:47:15', 6),
+(42, 'bull', 'living creature', 20, '2015-05-20 16:36:32', 6),
+(44, 'whaaat', 'IDK', 10, '2015-05-20 16:42:31', 6),
+(45, 'whaaat', 'IDK', 10, '2015-05-20 16:43:39', 6);
 
 -- --------------------------------------------------------
 
@@ -79,8 +84,8 @@ CREATE TABLE IF NOT EXISTS `role` (
 --
 
 INSERT INTO `role` (`roleID`, `role`) VALUES
-(1, 'admin'),
-(2, 'user');
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_USER');
 
 -- --------------------------------------------------------
 
@@ -93,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(25) NOT NULL,
   `roleID` int(11) NOT NULL,
   `username` varchar(25) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
@@ -102,7 +107,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`userID`, `password`, `roleID`, `username`) VALUES
 (6, 'hibernate', 2, 'spring'),
 (7, 'doe', 1, 'john'),
-(8, 'user', 2, 'user');
+(8, 'user', 2, 'user'),
+(10, 'hibernate', 1, 'springing'),
+(11, 'anklebreaker', 1, 'wickedzick');
 
 --
 -- Indexes for dumped tables
@@ -140,17 +147,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `bidhistory`
 --
 ALTER TABLE `bidhistory`
-  MODIFY `bidID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+  MODIFY `bidID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `itemId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=42;
+  MODIFY `itemId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=46;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
